1cd28b93e5de75fb579a3b23fea4e147f4cb7c14
tahara1212 <73981345+tahara1212@users.noreply.github.com>
2021-01-16 17:44:43 +0900

    day3_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./Components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  // #コンポーネント内で動的に値が切り替わる場合は、stateを使用する。
+  // #useStateから配列の分割代入で取り出す。
+  // #一つ目にstateとして使用する変数、二つ目にそれを更新する関数を定義。
+  // #useStateの括弧内に初期値を設定。
+  const [num, setnum] = useState(0);
+  const [faceShowFlg, setFaceShowFlg] = useState(true);
+  // #カウントアップボタン
+  const onClickCountUp = () => {
+    // #stateの更新。
+    setnum(num + 1);
+  };
+  // #表示、非表示ボタン
+  const onClickFaceShowFlg = () => {
+    setFaceShowFlg(!faceShowFlg);
+  };
+
+  // #stateが更新されると再レンダリングが発生し、意図しない処理が呼ばれる可能性がある。
+  // #レンダリング時に参照する値を決定し、その値に依存する処理を記述する。
+  // #設定した値が変動しない限り、useEffectの中の処理は実行されない。
+  useEffect(() => {
+    // #カウントアップが3の倍数の時のみ表示する。
+    if (num % 3 === 0) {
+      // #パイプラインは左がfalseの場合に右を実行する。
+      // #左がtrueの場合、右の処理は実行されない。
+      faceShowFlg || setFaceShowFlg(true);
+    } else {
+      // #アンパサンドは左がtrueの場合に右を実行する。
+      // #右がfalseの場合、左の処理は実行されない。
+      faceShowFlg && setFaceShowFlg(false);
+    }
+    // #第二引数に配列を設定し、参照する値を決定する。
+    // #空の配列を渡すと、最初の一回のみ実行する。
+    // #useEffect内で使用している変数がuseEffect内で定義されていない場合、eslintが警告を発する。
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     // #JSX記法は一つのタグで囲わないとエラーになる。
@@ -26,7 +60,10 @@ const App = () => {
       <ColorfulMessage color="black" message="Hello" />
       <ColorfulMessage color="red" message="idol" />
       <ColorfulMessage color="red">konnichiha</ColorfulMessage>
-      <button onClick={onClickButton}>判定</button>
+      <button onClick={onClickCountUp}>Count Up!</button>
+      <p>{num}</p>
+      <button onClick={onClickFaceShowFlg}>on/off</button>
+      {faceShowFlg && <p>（＾ω＾）</p>}
     </>
   );
 };
